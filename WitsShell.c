067be2f7eca81efc64147cebb82b95b9a777acdb
dean-south <solomon.mdean@gmail.com>
2023-08-16 13:39:47 +0200

    Update Path and fork/execv functionality implemented

--- WitsShell.c
+++ WitsShell.c
@@ -3,18 +3,24 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
 
 //I call this data structure a linked-array. It's an array with a pointer to the next array
-struct stringHolder
+struct StringHolder
 {
     char *string;
-    struct stringHolder *next_pointer;
+    struct StringHolder *next_pointer;
+    int numStrings;
 };
 
 // function to initialize the array-list and set the length of the string
-struct stringHolder *initStingHolder(const char *string, size_t string_size)
+struct StringHolder *initStingHolder(const char *string)
 {
-    struct stringHolder *holder = (struct stringHolder *)malloc(sizeof(struct stringHolder));
+    struct StringHolder *holder = (struct StringHolder *)malloc(sizeof(struct StringHolder));
+
+    size_t string_size = strlen(string);
 
     if (holder != NULL)
     {
@@ -29,15 +35,15 @@ struct stringHolder *initStingHolder(const char *string, size_t string_size)
 }
 
 // fucntion that adds a pointer to the next array-list
-void addPointer(struct stringHolder *holder, struct stringHolder *pointer){
+void addPointer(struct StringHolder *holder, struct StringHolder *pointer){
     holder->next_pointer = pointer;
 }
 
 // function that frees up the space in memory used for the array-list
-void freeStringHolder(struct stringHolder *holder)
+void freeStringHolder(struct StringHolder *holder)
 {
-    struct stringHolder *pointer = holder;
-    struct stringHolder *temp;
+    struct StringHolder *pointer = holder;
+    struct StringHolder *temp;
     while (pointer != NULL)
     {
         temp = pointer->next_pointer;
@@ -47,14 +53,61 @@ void freeStringHolder(struct stringHolder *holder)
     
 }
 
+void updatePath(struct StringHolder *head, struct StringHolder *path){
+    free(path);
+
+    path = initStingHolder(head->next_pointer->string);
+
+    struct StringHolder *pathPtr = path;
+
+    struct StringHolder *headPtr = head->next_pointer;
+
+    while (headPtr != NULL)         
+    {
+        addPointer(pathPtr, initStingHolder(headPtr->string));
+        
+        pathPtr = pathPtr->next_pointer;
+        headPtr = headPtr->next_pointer;
+    }
+}
+
+char* exit_statements[] = {"Fine leave me, like everyone else does\n", "Everyone eventually leaves me. You're no different\n", 
+"One day you'll realise how of a mistake it is to leave me\n", "Fine Leave me, I never liked you anyway\n", 
+"Hope you learn to cherish the time we spent together after you leave me\n"};
+
+char** getArg(struct StringHolder *pointer){
+    char** arg[pointer->numStrings - 1];
+    
+    if (pointer->numStrings > 1)
+    {
+
+        for (size_t i = 1; i < pointer->numStrings; i++){
+            pointer = pointer->next_pointer;
+
+            arg[i] = &pointer->string;
+        }
+        
+    }
+    
+    return arg;
+}
+
 
 int main(int argc, char const *argv[])
 {
+
+    struct StringHolder *path = initStingHolder("/bin/");
+
+    char *token, *rest;
+    const char *delim = " ";
+
+    srand(time(NULL));
+
+
     if (argc > 1){
         printf("%s", &argv);
 
         return 0;
-
     }
 
 
@@ -62,8 +115,6 @@ int main(int argc, char const *argv[])
     size_t len = 0;     // Initial size of buffer
     ssize_t read;
 
-    char *token, *rest;
-    const char *delim = " ";
 
     while (true)
     {
@@ -79,19 +130,20 @@ int main(int argc, char const *argv[])
 
             token = strsep(&rest, delim);
 
-            struct stringHolder *head = initStingHolder(token, strlen(token));
+            struct StringHolder *head = initStingHolder(token);
 
-            struct stringHolder *pointer = head;
+            head->numStrings = 1;
+
+            struct StringHolder *pointer = head;
 
             while ((token = strsep(&rest, delim)) != NULL)  
             {
-                
-                addPointer(pointer, initStingHolder(token, strlen(token)));
+                addPointer(pointer, initStingHolder(token));
+
+                head->numStrings++;
                 
                 pointer = pointer->next_pointer;
             }
-
-            pointer = head;
 
             //print array-list
             // while (pointer != NULL)
@@ -101,16 +153,56 @@ int main(int argc, char const *argv[])
             //     pointer = pointer->next_pointer;
             // }
 
-            if (strcmp(head->string, "exit") == 0 || 1==1)
+
+            if (strcmp(head->string, "exit") == 0)
             {
+                printf("%s", exit_statements[rand() % 5]);
                 exit(0);
-            } 
+            }
+            else if (strcmp(head->string, "path") == 0)
+            {
+                updatePath(head, path);
+            }
+            else if (strcmp(head->string, "cd") == 0)
+            {
+                printf("fuck off \n");
+            }
+            else
+            {
+                pointer = path;
+                while (pointer != NULL)
+                {
+                    char *str_search = strcat(pointer->string, head->string);
+                    if (access(str_search, X_OK) == 0)
+                    {
+
+                        int pid = fork();
+                        if (pid == 0){
+                            char** arg = getArg(head);
+                            execv(pointer->string, arg);
+                        }
+                        else {
+                            wait(NULL);
+                            break;
+                        }
+                        
+                    }
+
+                    pointer = pointer->next_pointer;
+
+                    
+                }
+                
+                
+                
+            }
+            
 
             freeStringHolder(head);
-            free(pointer);
+            //free(pointer);
 
         } else {
-            printf("\n");
+            printf("%s", exit_statements[rand()%5]);
             exit(0);
         }
 
--- test.c
+++ test.c
@@ -3,18 +3,20 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
 
 int main(int argc, char const *argv[])
 {
-    char input[] = "apple,banana,cherry,date";
-    char *token, *rest = input;
-    const char *delim = ",";
-
-    while ((token = strsep(&rest, delim)) != NULL) {
-        printf("%zu \n", strlen(token));
-        printf("Token: %s\n", token);
+    int pid = fork();
+    if(pid == 0){
+        return 69;
     }
-
-
+    else{
+        int res = -5;
+        wait(&res) ;
+    printf("the result was %d\n" , WEXITSTATUS(res));
+    }
     return 0;
 }
